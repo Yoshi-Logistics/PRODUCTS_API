b83450fb293da0dbd0261a615ee523a316c41d1a
kbakalova <bakalovaa@gmail.com>
2022-03-11 20:40:03 -0800

    loader.io tolken

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 var controllers = require('./controllers');
+const path = require('path');
 
 //connect to frontend here: (?)
 //app.use(express.static('client/dist'));
+app.get('/loaderio-fff75b162192bedf66f71ed992fdae08.txt', (req,res) => {
+  res.sendFile(path.resolve('loaderio-fff75b162192bedf66f71ed992fdae08.txt'))
+});
 
 app.get('/products', (req, res) => {
   controllers.products.getAll(req, res);
